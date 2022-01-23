acfcbf54af5ecbcd5a7ce46ffb44f1bf571fb319
Taeho <kimdogg911@gmail.com>
2022-01-23 20:32:07 +0900

    added horizontal bar chart

--- front/src/components/MoneyStat.js
+++ front/src/components/MoneyStat.js
@@ -1,20 +1,92 @@
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
-import { Doughnut } from 'react-chartjs-2'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Doughnut, Bar } from 'react-chartjs-2'
 import moneyModule from "../modules/moneyModule"
 
-ChartJS.register(ArcElement, Tooltip, Legend)
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend
+)
 
 function MoneyStat(props) {
   const [income, spending] = moneyModule.getIncomeAndSpending(props.moneyList)
+  const [eachTagKeys, eachTagValues] = moneyModule.getTotalAmountOfEachTag(props.moneyList)
+  const colorSet = [
+    'rgba(255, 99, 132, 0.5)',
+    'rgba(54, 162, 235, 0.5)',
+    'rgba(255, 206, 86, 0.5)',
+    'rgba(75, 192, 192, 0.5)',
+    'rgba(153, 102, 255, 0.5)',
+    'rgba(255, 159, 64, 0.5)'
+  ]
+  
+  const getColorArray = len => {
+    let arr = []
+    let loop = parseInt(len / colorSet.length)
+    let remain = len % colorSet.length
+    for (let i = 0; i < loop; i++) {
+      arr = arr.concat(colorSet)
+    }
+    arr = arr.concat(colorSet.slice(0, remain))
+    return arr
+  }
+
+  const doughnutOptions = {
+    plugins: {
+      title: {
+        display: true,
+        text: 'Income and Spending',
+        font: {size: 25}
+      }
+    }
+  }
+
+  const horizontalBarOptions = {
+    indexAxis: 'y',
+    elements: {
+      bar: {borderWidth: 2},
+    },
+    responsive: true,
+    plugins: {
+      legend: {display: false},
+      title: {
+        display: true,
+        text: 'Each Tags Stat',
+        font: {size: 25}
+      }
+    }
+  }
 
   const incomeSpendingData = {
     labels: ['Income', 'Spending'],
     datasets: [
       {
-        data: [moneyModule.getTotalAmount(income), moneyModule.getTotalAmount(spending)],
-        backgroundColor: ['#1a73e8', '#ff3e00'],
-        borderColor: ['blue', 'red'],
-        borderWidth: 1
+        data: [income, spending],
+        backgroundColor: ['rgba(53, 162, 235, 0.5)', 'rgba(255, 99, 132, 0.5)'],
+        borderWidth: 5
+      }
+    ]
+  }
+
+  const eachTagData = {
+    labels: eachTagKeys,
+    datasets: [
+      {
+        data: eachTagValues,
+        backgroundColor: getColorArray(eachTagValues.length)
       }
     ]
   }
@@ -22,7 +94,12 @@ function MoneyStat(props) {
   return (
     <>
       <div>
-        <Doughnut data={incomeSpendingData} />
+        <hr className='margintop4 marginbottom4'></hr>
+        <Doughnut data={incomeSpendingData} options={doughnutOptions} />
+        <hr className='margintop4 marginbottom4'></hr>
+        <Bar data={eachTagData} options={horizontalBarOptions} />
+        <hr className='margintop4 marginbottom4'></hr>
+        <button onClick={e => console.log(moneyModule.getTotalAmountOfEachTag(props.moneyList))}>getTotalAmountOfEachTag</button>
       </div>
     </>
   )
--- front/src/modules/moneyModule.js
+++ front/src/modules/moneyModule.js
@@ -1,20 +1,43 @@
 export default {
   getIncomeAndSpending: moneyList => {
-    let income = []
-    let spending = []
+    let income = 0
+    let spending = 0
     moneyList.map(money => {
       if (money.amount >= 0) {
-        income.push(money)
+        income += money.amount
       } else {
-        spending.push(money)
+        spending += money.amount
       }
     })
     return [income, spending]
   },
-  
+
   getTotalAmount: moneyList => {
     let total = 0
     moneyList.map(money => total += money.amount)
     return total
+  },
+
+  getTotalAmountOfEachTag: moneyList => {
+    let totals = new Map()
+    moneyList.map(money => {
+      if (money.tags.length == 0) {
+        let key = 'extra'
+        if (totals.has(key)) {
+          totals.set(key, totals.get(key) + money.amount)
+        } else {
+          totals.set(key, money.amount)
+        }
+      } else {
+        money.tags.map(tag => {
+          if (totals.has(tag)) {
+            totals.set(tag, totals.get(tag) + money.amount)
+          } else {
+            totals.set(tag, money.amount)
+          }
+        })
+      }
+    })
+    return [Array.from(totals.keys()), Array.from(totals.values())]
   }
 }
